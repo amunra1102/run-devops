d9e941a72390b00263040a356262650e12166aa1
Thanh Van Vu <thanh.vu@hitachivantara.com>
2021-01-30 21:46:19 +0700

    format code

--- Shopping.Client/Data/ProductContext.cs
+++ Shopping.Client/Data/ProductContext.cs
@@ -9,51 +9,51 @@ namespace Shopping.Client.Data
         public static List<Product> Products = new List<Product> {
             new Product()
                 {
-                    Name = "IPhone X",
+                    Name        = "IPhone X",
                     Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
-                    ImageFile = "product-1.png",
-                    Price = 950.00M,
-                    Category = "Smart Phone"
+                    ImageFile   = "product-1.png",
+                    Price       = 950.00M,
+                    Category    = "Smart Phone"
                 },
                 new Product()
                 {
-                    Name = "Samsung 10",
+                    Name        = "Samsung 10",
                     Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
-                    ImageFile = "product-2.png",
-                    Price = 840.00M,
-                    Category = "Smart Phone"
+                    ImageFile   = "product-2.png",
+                    Price       = 840.00M,
+                    Category    = "Smart Phone"
                 },
                 new Product()
                 {
-                    Name = "Huawei Plus",
+                    Name        = "Huawei Plus",
                     Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
-                    ImageFile = "product-3.png",
-                    Price = 650.00M,
-                    Category = "White Appliances"
+                    ImageFile   = "product-3.png",
+                    Price       = 650.00M,
+                    Category    = "White Appliances"
                 },
                 new Product()
                 {
-                    Name = "Xiaomi Mi 9",
+                    Name        = "Xiaomi Mi 9",
                     Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
-                    ImageFile = "product-4.png",
-                    Price = 470.00M,
-                    Category = "White Appliances"
+                    ImageFile   = "product-4.png",
+                    Price       = 470.00M,
+                    Category    = "White Appliances"
                 },
                 new Product()
                 {
-                    Name = "HTC U11+ Plus",
+                    Name        = "HTC U11+ Plus",
                     Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
-                    ImageFile = "product-5.png",
-                    Price = 380.00M,
-                    Category = "Smart Phone"
+                    ImageFile   = "product-5.png",
+                    Price       = 380.00M,
+                    Category    = "Smart Phone"
                 },
                 new Product()
                 {
-                    Name = "LG G7 ThinQ EndofCourse",
+                    Name        = "LG G7 ThinQ EndofCourse",
                     Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
-                    ImageFile = "product-6.png",
-                    Price = 240.00M,
-                    Category = "Home Kitchen"
+                    ImageFile   = "product-6.png",
+                    Price       = 240.00M,
+                    Category    = "Home Kitchen"
                 }
         };
     }
